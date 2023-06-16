e65d0d34514164e1d215a61db2055c2f0b464516
iGerman00 <igermanproduction@gmail.com>
2023-06-16 19:42:49 +0000

    Some more error handling

--- app.py
+++ app.py
@@ -31,31 +31,41 @@ for style in Style:
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        prompt = request.form["prompt"]
-        negative = request.form["negative"]
-        style = Style[request.form["style"]]
-        ratio = Ratio[request.form["ratio"]]
-        steps = int(request.form["steps"]) 
-        high_res = bool(request.form.get("high_res", False)) 
-        upscale = bool(request.form.get("upscale", False))
-        cfg = float(request.form["cfg"])
+        try:
+            message = "Image generated successfully"
+            prompt = request.form["prompt"]
+            negative = request.form["negative"]
+            style = Style[request.form["style"]]
+            ratio = Ratio[request.form["ratio"]]
+            steps = int(request.form["steps"]) 
+            high_res = bool(request.form.get("high_res", False)) 
+            upscale = bool(request.form.get("upscale", False))
+            cfg = float(request.form["cfg"])
 
-        # high_res needs to be 0 or 1 for the api
-        high_res = 1 if high_res else 0
+            # high_res needs to be 0 or 1 for the api
+            high_res = 1 if high_res else 0
 
-        # Generate random seed number that can go up to 999999999
-        seed = int.from_bytes(os.urandom(4), byteorder="big") % 999999999
+            # Generate random seed number that can go up to 999999999
+            seed = int.from_bytes(os.urandom(4), byteorder="big") % 999999999
 
-        img_data = imagine.sdprem(prompt=prompt, style=style, ratio=ratio, high_res_results=high_res, negative=negative, cfg=cfg, seed=seed, priority=1, steps=steps)
+            img_data = imagine.sdprem(prompt=prompt, style=style, ratio=ratio, high_res_results=high_res, negative=negative, cfg=cfg, seed=seed, priority=1, steps=steps)
 
-        if upscale and img_data is not None:
-            img_data = imagine.upscale(image=img_data)
+            if img_data is None:
+                return make_response({"status": "error", "message": "No image data returned from API"}, 500)
 
-        if img_data is not None:
-            return make_response({"status": "success", "img_data": base64.encodebytes(img_data).decode("utf-8")})
+            if upscale is not None:
+                img_data_upscaled = imagine.upscale(image=img_data)
+            
+            if img_data_upscaled is not None:
+                img_data = img_data_upscaled
+            else:
+                message = "Upscaling failed, using low res image"
 
-        else:
-            return make_response({"status": "error"}, 500)
+            if img_data is not None:
+                return make_response({"status": "success", "img_data": base64.encodebytes(img_data).decode("utf-8"), "message": message})
+
+        except Exception as e:
+            return make_response({"status": "error", "message": str(e)}, 500)
 
     return render_template("index.html", styles=Style, ratios=Ratio)
 
